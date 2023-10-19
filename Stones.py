ae651d4f667c0f1adc3b9442cb9610e5a66d8dfb
GionaCantarutti <cantarutti.giona@spes.uniud.it>
2023-10-19 13:55:49 +0200

    Useful comments

--- Stones.py
+++ Stones.py
@@ -13,5 +13,9 @@ MEDIUM_TILECOUNT = 100
 HARD_SIZE = 15
 HARD_TILECOUNT = 120
 
+#MiniZincB performa molto meglio quando gli vengono date abbastanza tiles. Oltre ad un certo punto aumentare le tiles smette di avere un costo sulla performance
+#MiniZincB andrà in timeout per n=11 con 70 tiles (poco più del minimo necessario per coprire la grid) ma riesce a risolvere l'istanza n11 con 120 tiles in soli 25 secondi
+#MiniZincA probabilmente non funziona alla stessa maniera, da testare
+
 newInstance = StoneInstance.generateRandom(MEDIUM_SIZE, MEDIUM_TILECOUNT)
 print((MiniZincB.solveInstance(newInstance, TIMEOUT)).solutionString())
