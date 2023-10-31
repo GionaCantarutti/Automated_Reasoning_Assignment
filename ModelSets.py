0271974456fc6a64fde1f78e980a1d77f01a2bc3
GionaCantarutti <cantarutti.giona@spes.uniud.it>
2023-10-31 12:38:34 +0100

    More benchmakrs and setup for final test

--- ModelSets.py
+++ ModelSets.py
@@ -3,8 +3,15 @@ from MiniZincB import *
 
 TOTAL_TIME_BUDGET_PER_SOLVER = 13 * 5 * 60
 
+FINAL_TESTING_C = [
+    MiniZincB("Models/Minizinc/Best model candidate A.mzn", "best model candidate A", TOTAL_TIME_BUDGET_PER_SOLVER),
+    MiniZincB("Models/Minizinc/Best model candidate B.mzn", "best model candidate B", TOTAL_TIME_BUDGET_PER_SOLVER),
+    MiniZincB("Models/Minizinc/Final models/dom_w_deg + indomain_random.mzn", "dom_w_deg + indomain_random", TOTAL_TIME_BUDGET_PER_SOLVER),
+    MiniZincB("Models/Minizinc/Final models/input_order + indomain_min.mzn", "input_order + indomain_min", TOTAL_TIME_BUDGET_PER_SOLVER),
+]
+
 FINAL_TESTING_B = [
-    MiniZincB("Models/Minizinc/Final models/Best model.mzn", "best model candidate", TOTAL_TIME_BUDGET_PER_SOLVER),
+    MiniZincB("Models/Minizinc/Best model candidate A.mzn", "best model candidate", TOTAL_TIME_BUDGET_PER_SOLVER),
     MiniZincB("Models/Minizinc/Improved coordinates/Keep chance/luby 15 conditional.mzn", "dom_w_deg + indomain_min", TOTAL_TIME_BUDGET_PER_SOLVER),
     MiniZincB("Models/Minizinc/Final models/dom_w_deg + indomain_random.mzn", "dom_w_deg + indomain_random", TOTAL_TIME_BUDGET_PER_SOLVER),
     MiniZincB("Models/Minizinc/Final models/input_order + indomain_min.mzn", "input_order + indomain_min", TOTAL_TIME_BUDGET_PER_SOLVER),
