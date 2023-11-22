fc8a16a8ac70e465b92915bae4a9ef4605e75dde
GionaCantarutti <cantarutti.giona@spes.uniud.it>
2023-11-22 09:37:41 +0100

    Benchmark prep

--- ModelSets.py
+++ ModelSets.py
@@ -6,6 +6,7 @@ TOTAL_TIME_BUDGET_PER_SOLVER = 3 * 5 * 60
 
 SYMMETRY_TESTING = [
     Clingo("Models/ASP/symmetry_breaking.lp", "Clingo symmetry test", TOTAL_TIME_BUDGET_PER_SOLVER),
+    Clingo("Models/ASP/follows_model.lp", "Clingo follows-based model", TOTAL_TIME_BUDGET_PER_SOLVER),
 ]
 
 CLINGO_COMPARASON = [
